923aa02d1dba88a52c6076de2de879613415c64c
jaibae21 <tonkasmakai@gmail.com>
2022-06-16 17:27:17 -0500

    Adding game
    Should have faby, background, and ground along with movement script which only contains input grab

---
+++ Project_1/Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float gravity = -9.8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per graphical frame
+    void Update()
+    {
+        //up because simulating tapping
+        bool isTapping = Input.GetKeyUp(KeyCode.Space);
+
+    }
+}
